46884e92136dedff1105634040de1666a1d9d12b
aden.j <aden.j@kakaocorp.com>
2024-07-01 20:12:14 +0900

    apigateway

--- apigateway-service/src/main/kotlin/com/example/apigatewayservice/ApigatewayServiceApplication.kt
+++ apigateway-service/src/main/kotlin/com/example/apigatewayservice/ApigatewayServiceApplication.kt
@@ -2,10 +2,21 @@ package com.example.apigatewayservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.boot.actuate.web.exchanges.HttpExchangeRepository
+import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository
 
 @SpringBootApplication
-class ApigatewayServiceApplication
+class ApigatewayServiceApplication {
+    @Bean
+    fun httpTraceRepository(): HttpExchangeRepository {
+        return InMemoryHttpExchangeRepository()
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<ApigatewayServiceApplication>(*args)
+
 }
+
+
